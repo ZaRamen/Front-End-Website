a293873762b94dc576c19643480053db1a8843c2
Raymond Lin <86033049+ZaRamen@users.noreply.github.com>
2022-10-31 12:48:14 -0400

    Update script.js

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ window.addEventListener('DOMContentLoaded', (event) => {
     {
 
         console.log(document.body.style.backgroundColor);
-        if (document.body.style.backgroundColor != "beige")
+        if (document.body.style.backgroundColor != "black")
         {
-            document.body.style.backgroundColor = "beige";
+            document.body.style.backgroundColor = "black";
         }
         else
         {
