94bf53ce07d458f17161b8c54bb47be1c9a80e12
ZaRamen <indead991@gmail.com>
2022-11-03 20:11:31 -0400

    Fixed link being blue when unvisited

--- script.js
+++ script.js
@@ -1,22 +1,42 @@
-window.addEventListener('DOMContentLoaded', (event) => {
+
+function initialize()
+{
+    bodyStyle = document.body.style;
+    footer = document.getElementById("footer");
+    // if (isDark)
+    // {
+    //     setMode();
+    // }
+}
+function setMode()
+{
+    if (bodyStyle.backgroundColor != "rgb(51, 58, 86)")
+    {
+        bodyStyle.backgroundColor = "#333A56";
+        bodyStyle.color = "#FFFFFF";
+        footer.style.color = "white";
+        footer.style.backgroundColor = "#39445c";
+        isDark = true;
+    }
+    else
+    {   
+        bodyStyle.backgroundColor = "#F7F5E6";
+        bodyStyle.color = "black";
+        footer.style.color = "black";
+        footer.style.backgroundColor = "#E8E8E8";
+        isDark = false;
+    }
+}
+window.addEventListener('DOMContentLoaded', (event) => 
+{
     console.log('DOM fully loaded and parsed');
+    
     document.getElementById('mode').addEventListener("click", (event) => 
     {
-        
-        var bodyStyle = document.body.style;
+      
         // returns rgb instead of hex //also can't set var to document.body.style.backgroundColor
         console.log(bodyStyle.backgroundColor)
-        if (bodyStyle.backgroundColor != "rgb(51, 58, 86)")
-        {
-            bodyStyle.backgroundColor = "#333A56";
-            bodyStyle.color = "#FFFFFF";
-        }
-        else
-        {   
-            bodyStyle.backgroundColor = "#F7F5E6";
-            bodyStyle.color = "black";
-        }
-        
 
+        setMode();
     });
 });
