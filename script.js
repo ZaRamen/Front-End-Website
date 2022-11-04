ca79e5a49839b6db6c73964a2eb8dab6fc8e2fb6
ZaRamen <indead991@gmail.com>
2022-11-03 22:12:32 -0400

    Added dark mode stays on for all web pages

--- script.js
+++ script.js
@@ -1,38 +1,52 @@
 
-let isDark = false;
-
 function initialize()
 {
     bodyStyle = document.body.style;
     footer = document.getElementById("footer-content");
-    if (isDark)
+    // If the previous page was dark then continue to be dark mode and vice versa
+    if (localStorage.getItem("isDark") == "true")
     {
-        setMode();
+        darkMode();
+    }
+    else
+    { 
+        lightMode();
     }
 }
 function setMode()
 {
-    if (bodyStyle.backgroundColor != "rgb(51, 58, 86)")
+    // If it's dark mode switch to light mode and vice versa
+    if (bodyStyle.backgroundColor == "rgb(51, 58, 86)")
     {
-        bodyStyle.backgroundColor = "#333A56";
-        bodyStyle.color = "#FFFFFF";
-        footer.style.color = "white";
-        footer.style.backgroundColor = "#39445c";
-        isDark = true;
+        lightMode();
     }
     else
     {   
-        bodyStyle.backgroundColor = "#F7F5E6";
-        bodyStyle.color = "black";
-        footer.style.color = "black";
-        footer.style.backgroundColor = "#E8E8E8";
-        isDark = false;
+        darkMode();
     }
+}
+function lightMode()
+{
+    console.log("I'm" + localStorage.getItem("isDark"));
+    bodyStyle.backgroundColor = "#F7F5E6";
+    bodyStyle.color = "black";
+    footer.style.color = "black";
+    footer.style.backgroundColor = "#E8E8E8";
+    // stores a variable named isDark between all web pages 
+    localStorage.setItem("isDark", false);
+}
+function darkMode()
+{
+    bodyStyle.backgroundColor = "#333A56";
+    bodyStyle.color = "#FFFFFF";
+    footer.style.color = "white";
+    footer.style.backgroundColor = "#39445c";
+    localStorage.setItem("isDark", true);
 }
 window.addEventListener('DOMContentLoaded', (event) => 
 {
     console.log('DOM fully loaded and parsed');
-    
+    initialize();
     document.getElementById('mode').addEventListener("click", (event) => 
     {
       
